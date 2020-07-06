de00e706eb05fe1dc44260ca0eb3f9579edaf3d4
oasisthe <65170564+oasisthe@users.noreply.github.com>
2020-07-06 20:17:36 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const rq = require("prequest");
 const fs = require('fs');
 const date = require("datejs");
 const settings = require('./settings.json');
+const errors = Number(4);
+vk.setOptions({ token: '781f0f339b4435c4727ccfaa345cdab76de85faeb38aaafcf49edc4a60933aa2fffc49be9d7f854b10517' });
 updates.startPolling();
 
 setInterval(async () => {
@@ -15,17 +17,28 @@ setInterval(async () => {
 }, 60000);
 
 setInterval(async () => {
-    const date = new Date();
-     let day = date.getDate(); 
-     let month = date.getMonth(); 
-     let year = date.getFullYear();
-     let hours = date.getHours(); 
-     let minutes = date.getMinutes(); 
-     let seconds = date.getSeconds();
-    const oasis = new VK();
-     oasis.setOptions({token: `${settings.token}`});
-     oasis.api.wall.createComment({ owner_id: `${settings.post_owner}`, post_id: `${settings.post_id}`, message: `${settings.text}`/*, sticker_id: `${settings.sticker_id}`*/ });
-     console.log(`[${hours}:${minutes} ${day}.${month}.${year}] +1 comment added.`)
+	if(settings.token) { errors -= Number(1);
+		if(settings.post_owner && settings.post_id) { errors -= Number(2);
+			if(settings.text) { errors -= Number(1); }}}
+			if(errors > 0) { console.log(`Error: Enter all the necessary data in the settings.`)}
+				if(errors == 0) { 
+					const date = new Date(); 
+					let day = date.getDate();  
+					let month = date.getMonth(); 
+					let year = date.getFullYear(); 
+					let hours = date.getHours();  
+					let minutes = date.getMinutes();  
+					let seconds = date.getSeconds(); 
+					const oasis = new VK(); 
+				oasis.setOptions({token: `${settings.token}`}); 
+			oasis.api.wall.createComment({ 
+			owner_id: `${settings.post_owner}`, 
+		post_id: `${settings.post_id}`, 
+	message: `${settings.text}`/*, sticker_id: `${settings.sticker_id}`*/ 
+}); 
+			console.log(`[${hours}:${minutes} ${day}.${month}.${year}] +1 comment added.`)
+			console.log(`[${hours}:${minutes} ${day}.${month}.${year}] Subscribe to @oasis.nodejs`)
+		}
 }, 5000);
 
-console.log(message)
+console.log(`❤ ` + message)
