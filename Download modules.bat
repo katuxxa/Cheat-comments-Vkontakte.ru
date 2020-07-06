cefa5786b884b19098df570ef75659a06d650a40
oasisthe <65170564+oasisthe@users.noreply.github.com>
2020-07-06 22:50:29 +0300

    Add files via upload

--- Download modules.bat
+++ Download modules.bat
@@ -2,4 +2,6 @@ start https://vk.com/id296538486
 start https://vk.com/public171960218
 start https://github.com/oasisthe
 start https://yadi.sk/d/MAYe1EdxDGch0Q
-i love you
+
+Forgive me for opening three more sites :(
+Прости что открыл тебе ещё три сайта :(
